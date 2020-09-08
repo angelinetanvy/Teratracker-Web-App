7bbd1d75ccbc8c88bdd5d5c1fade987469e0d946
msha0080 <msha0080@student.monash.edu>
2020-09-08 23:51:38 +0700

    SignUpPage updated 2

---
+++ codes/signup/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import login, authenticate
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2", "first_name", "last_name", "date_joined", "email"]
+
