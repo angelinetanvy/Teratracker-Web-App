01ff817ffcb28bb747607ccfd17ecd6b93b9570d
msha0080 <msha0080@student.monash.edu>
2020-09-08 22:34:28 +0700

    Register Page update

--- codes/signup/views.py
+++ codes/signup/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, World. Polls index")
+def signup(response):
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(response, "SignUpPage/SignUpPage.html", {"form": form})
