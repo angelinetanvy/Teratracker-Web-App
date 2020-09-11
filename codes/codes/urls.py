43330d45617ad832f55de12863df53d7fae70656
atan0064 <atan0064@student.monash.edu>
2020-09-11 14:22:59 +0700

    signin commit 2

--- codes/codes/urls.py
+++ codes/codes/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include("main.urls")),
     path('signup/', include('signup.urls')),
+    path('signin/',include('signin.urls')),
     path('',include("django.contrib.auth.urls"))
 ]
