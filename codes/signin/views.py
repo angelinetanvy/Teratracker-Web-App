c0789765b06652422424c9a1a47ac312c2dae4d0
msha0080 <msha0080@student.monash.edu>
2020-09-11 14:44:51 +0700

    AuthenticationForm

--- codes/signin/views.py
+++ codes/signin/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def signin(response):
     if response == "POST":
-        form = UserCreationForm(response.POST)
+        form = AuthenticationForm(response.POST)
         if form.is_valid():
             form.save()
     else:
-        form = UserCreationForm()
+        form = AuthenticationForm()
     return render(response, "signin/signin.html", {"form": form})
